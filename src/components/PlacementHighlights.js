1cfb872fb5165e02ce63190d43a60281e8c6a9c8
atulsadiwal <atul.sadiwal@brandshow.in>
2024-12-03 17:36:09 +0530

    changed

--- src/components/PlacementHighlights.js
+++ src/components/PlacementHighlights.js
@@ -20,14 +20,6 @@ const placement4 = [
 export default function PlacementHighlights() {
     return (
         <div className="grid grid-cols-1 xl:grid-cols-2 h-auto max-sm:px-2">
-            <div className="flex flex-col justify-center">
-                <div className="grid grid-cols-1 sm:grid-cols-2 xl:place-content-start max-xl:place-content-center max-sm:gap-2 h-full max-sm:mb-1.5">
-                    <PlacementSlider placement={placement1} delay={4000} />
-                    <PlacementSlider placement={placement2} delay={3000} />
-                    <PlacementSlider placement={placement3} delay={3500} />
-                    <PlacementSlider placement={placement4} delay={4500} />
-                </div>
-            </div>
             <div className="flex flex-col justify-center p-5 md:p-10 lg:p-16 bg-BG1 bg-center bg-cover text-white bg-black bg-blend-multiply bg-opacity-75">
                 <h2 className="text-3xl md:text-4xl lg:text-5xl font-novaLight">
                     <span className='font-novaSemi bg-gradient-to-r from-green-500 to-indigo-400 bg-clip-text text-transparent animate-gradient'>Placements</span> Overview
@@ -75,6 +67,14 @@ export default function PlacementHighlights() {
                 <div className="flex justify-start space-x-4 mt-6 max-lg:justify-center">
                     <button className="bg-btn-gradient text-white text-sm font-novaBold px-6 py-2 max-[360px]:text-[11px] rounded-md hover:bg-[#3c5686] hover:border-b-4 hover:border-[#ccce48] hover:transform hover:transition-transform hover:ease-in-out hover:duration-500 scale-y-105">APPLY TODAY</button>
                     <button className="border border-white text-white text-sm font-novaBold px-4 py-2 max-[360px]:text-[11px] rounded-md hover:bg-white hover:text-black">VIEW PLACEMENTS</button>
+                </div>
+            </div>
+            <div className="flex flex-col justify-center">
+                <div className="grid grid-cols-1 sm:grid-cols-2 xl:place-content-start max-xl:place-content-center max-sm:gap-2 h-full max-sm:mb-1.5">
+                    <PlacementSlider placement={placement1} delay={4000} />
+                    <PlacementSlider placement={placement2} delay={3000} />
+                    <PlacementSlider placement={placement3} delay={3500} />
+                    <PlacementSlider placement={placement4} delay={4500} />
                 </div>
             </div>
         </div>
