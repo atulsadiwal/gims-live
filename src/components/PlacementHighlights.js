519e6ae3b0cca776cbe3ae81980223be018a2f44
JatinBrandshow <jatin.yadav@brandshow.in>
2024-12-21 16:56:31 +0530

    New Changes

--- src/components/PlacementHighlights.js
+++ src/components/PlacementHighlights.js
@@ -1,20 +1,20 @@
 import PlacementSlider from "./PlacementSlider";
 
 const placement1 = [
-    "/image/student/abhay-kumar.jpg",
-    "/image/student/ankur-kumar-mishra.jpg",
+    "/image/student/abhay-kumar.webp",
+    "/image/student/ankur-kumar-mishra.webp",
 ]
 const placement2 = [
-    "/image/student/dilpreet-singh.jpg",
-    "/image/student/hanshika-bhushan.jpg",
+    "/image/student/dilpreet-singh.webp",
+    "/image/student/hanshika-bhushan.webp",
 ]
 const placement3 = [
-    "/image/student/raksha-shreya.jpg",
-    "/image/student/ritika-saxena.jpg",
+    "/image/student/raksha-shreya.webp",
+    "/image/student/ritika-saxena.webp",
 ]
 const placement4 = [
-    "/image/student/sunaina-thakur.jpg",
-    "/image/student/vivek-yadav.jpg",
+    "/image/student/sunaina-thakur.webp",
+    "/image/student/vivek-yadav.webp",
 ]
 
 export default function PlacementHighlights() {
